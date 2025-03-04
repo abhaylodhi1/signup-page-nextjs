3d4368e0ffe2b9964f81cc9d9b061e8fb4d4b6b4
Abhay_Lodhi <abhaylodhi2001@gmail.com>
2025-03-04 11:19:31 +0530

    feat: add commitlint setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { useCallback } from 'react';
+
 import './globals.css';
 
+import { Geist, Geist_Mono } from 'next/font/google';
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-///
-const geistMono = Geist_Mono({
-
 
 
   
+  variable: '--font-geist-sans',
+
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
